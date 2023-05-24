664a3a2bc49795ffd76bd5e50228e293d431980b
annamalairv <annamalai.rv@gmail.com>
2023-05-25 00:18:15 +0530

    my dashboard

--- components/Barchart.tsx
+++ components/Barchart.tsx
@@ -20,38 +20,47 @@ ChartJS.register(
 );
 
 const Barchart = () => {
-  const [chartData, setChartData] = useState({
-    datasets: [],
-  });
+  
+  // const [chartOptions, setChartOptions] = useState({});
 
-  const [chartOptions, setChartOptions] = useState({});
+  const chartData={
+    labels: ['week1', 'week2', 'week3', 'week4', 'week5','week6',"week7"],
+    datasets: [
+        {
+            label: 'Sales $',
+            data: [12, 28,6,13,23,13,20,17],
+            borderColor: 'rgb(254, 198, 1)',
+            backgroundColor: 'rgb(254,198,1)',
+          }, 
+    ] 
+  }
+  const chartOptions = {
+    responsive: true,
+    legend: {
+      display: true,
+      position: 'top',
+    },
+    title: {
+      display: true,
+      fontSize: 20,
+      text: 'Daily Revenue',
+      
+    },
+    maintainAspectRatio: false,
 
-  useEffect(() => {
-    setChartData({
-        labels: ['week1', 'week2', 'week3', 'week4', 'week5','week6',"week7"],
-        datasets: [
-            {
-                label: 'Sales $',
-                data: [12, 28,6,13,23,13,20,17],
-                borderColor: 'rgb(254, 198, 1)',
-                backgroundColor: 'rgb(254,198,1)',
-              }, 
-        ]
-    })
-    setChartOptions({
-        plugins: {
-            legend: {
-                position: 'top',
-            },
-            title: {
-                display: true,
-                text: 'Daily Revenue'
-            }
-        },
-        maintainAspectRatio: false,
-        responsive: true
-    })
-  }, [])
+  }
+//   const chartOptions={  plugins: {
+//     legend: {
+//         position: 'top',
+//     },
+//     title: {
+//         display: true,
+//         text: 'Daily Revenue'
+//     }
+// },
+
+// }
+ 
 
   return (
     <>
